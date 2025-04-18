3bee30f91f4f6f172b7d7d473a4bd73ddcc2b6e8
priyanshu kumar <dlnpriyanshu@gmail.com>
2025-04-18 18:41:02 +0530

    home page improved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Impact from '../components/Impact';
 
 const Home = () => {
   return (
-    <div className="w-full h-auto border-l-2 border-r-2 border-white max-[767px]:border-l-0 max-[767px]:border-r-0">
+    <div className="w-full h-auto border-l-2 border-r-2 border-[#7134a289] max-[767px]:border-l-0 max-[767px]:border-r-0">
     <Hero />
     <Discover />
     <Intro />
